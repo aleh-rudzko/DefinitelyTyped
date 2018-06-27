2ef871dc9fc4c0795998db56a2472a9c95b14d11
Oleg Rudko <oleg.rudko@skywindgroup.com>
2018-06-27 12:03:40 +0300

    add types for FactoryGirl

--- types/factory-girl/index.d.ts
+++ types/factory-girl/index.d.ts
@@ -3,14 +3,14 @@
 // Definitions by: Aleh Rudzko <https://github.com/aleh-rudzko>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare type Initializer<T> = (buildOptions?: any) => T;
+type Initializer<T> = (buildOptions?: Partial<T>) => T;
 
-export interface Options<T, M, I> {
+interface Options<T, M, I> {
     afterBuild(Model: M, attrs?: any, buildOptions?: Partial<T>): Promise<I>;
     afterCreate(Model: M, attrs?: any, buildOptions?: Partial<T>): Promise<I>;
 }
 
-export interface ExtendOptions<T, M, I> extends Options<T, M, I> {
+interface ExtendOptions<T, M, I> extends Options<T, M, I> {
     model?: M;
 }
 
@@ -18,29 +18,26 @@ type Partial<T> = {
     [P in keyof T]?: T[P];
 }
 
-export default class FactoryGirl {
+export declare class FactoryGirl {
+    private options: any;
     constructor(options?: any);
 
-    define<T, M, I>(name: string, Model: M, initilizer: T | Initializer<T>, options?: Options<T, M, I>): Factory<T, M>;
-    extend<T, M, I>(parent: string, name: string, childInitializer: T | Initializer<T>, options?: ExtendOptions<T, M, I>): Factory<T, M>;
+    define<T, M, I>(name: string, Model: M, initilizer: T | Initializer<T>, options?: Options<T, M, I>): any;
+    extend<T, M, I>(parent: string, name: string, childInitializer: T | Initializer<T>, options?: ExtendOptions<T, M, I>): any;
     attrs<T>(name: string, attrs: any, buildOptions?: Partial<T>): Promise<T>;
     build<T, I>(name: string, attrs: any, buildOptions?: Partial<T>): Promise<I>;
     create<T, I>(name: string, attrs?: any, buildOptions?: Partial<T>): Promise<I>;
     attrMany<T>(name: string, num: number, attrs: any[], buildOptions?: Partial<T>[]): Promise<T[]>;
     buildMany<T, I>(name: string, num: number, attrs: any[], buildOptions?: Partial<T>[]): Promise<I[]>;
     createMany<T, I>(name: string, num: number, attrs: any[], buildOptions?: Partial<T>[]): Promise<I[]>;
-    withOptions<T, M, I>(options: Options<T, M, I>, merge?: boolean);
-    setAdapter(adapter: any, factoryNames?: string | string[]);
+    withOptions<T, M, I>(options: Options<T, M, I>, merge?: boolean): void;
+    setAdapter(adapter: any, factoryNames?: string | string[]): void;
 
     assoc<T, I, K extends keyof I>(name: string, key?: K, attrs?: any, buildOptions?: Partial<T>): (name: string, key?: K, attrs?: any, buildOptions?: Partial<T>) => Promise<I | I[K]>;
     assocMany<T, I, K extends keyof I>(name: string, key?: K[], attrs?: any[], buildOptions?: Partial<T>[]): (name: string, key?: K[], attrs?: any[], buildOptions?: Partial<T>[]) => Promise<I[] | I[K][]>;
     sequence(id?: string, callback?: (next: number) => number): (id?: string, callback?: (next: number) => number) => number;
 }
 
-
-export class Factory<T, M> {
-    constructor(Model: M, initilizer: T | Initializer<T>, options?: any);
-
-    getFactoryAttrs(buildOptions?: any): Promise<T>;
-    attrs(extraAttrs?: any, buildOptions?: any): Promise<T>;
-}
+declare const factory: FactoryGirl;
+export { factory };
+export default factory;
